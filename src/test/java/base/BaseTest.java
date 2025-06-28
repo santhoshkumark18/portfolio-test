a597d4eb0211ad97f08af16d82b95ab92b8fb302
sandysk18 <santhoshinreallife@gmail.com>
2025-06-28 20:41:44 +0530

    base.java update

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,20 +1,31 @@
 package base;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-    protected WebDriver driver;
+    public WebDriver driver;
 
-    @BeforeMethod
+ 
+	@BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver(); // Or use WebDriverManager if preferred
-        driver.manage().window().maximize();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://santhoshkumark.me");
     }
 
-    @AfterMethod
+
+	@AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
